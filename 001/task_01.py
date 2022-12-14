0a09dd38b0da8feb8065bd6ddb1f461154a5da0f
Martin House <torchester@gmail.com>
2022-12-14 13:12:28 +0500

    add task 1

---
+++ 001/task_01.py
@@ -0,0 +1,20 @@
+
+#? 1. Напишите программу, которая принимает на вход цифру,
+#?    обозначающую день недели, и проверяет, является ли этот день выходным.
+
+# Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+day = int(input('Enter the day of the week from 1 to 7: '))
+
+if 0 < day < 8:
+
+    if day < 6:
+        print("No. It's a work day")
+    else:
+        print("Yes! It's weekend day")
+
+else:
+    print("It's not a day of the week")

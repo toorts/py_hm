efd73425db9134bee6782461bf9c82d5470fcf3c
Martin House <torchester@gmail.com>
2022-12-26 17:29:05 +0500

    add task 3.2

--- 003/task_02.py
+++ 003/task_02.py
@@ -9,5 +9,12 @@ from random import randint
 
 num = int(input('Enter the number of elements: '))
 
-lst = [randint(0, 9) for _ in range(num)]
+lst = [randint(1, 9) for _ in range(num)]
 print(lst)
+
+prod_lst = []
+
+for i in range(len(lst) // 2 + len(lst) % 2):
+    prod_lst.append(lst[i] * lst[-(i + 1)])
+
+print(prod_lst)

d089810304d3a8003f3894648aeca893fdd6435f
Martin House <torchester@gmail.com>
2022-12-28 15:41:46 +0500

    upd task 4.4

--- 004/task_04.py
+++ 004/task_04.py
@@ -3,26 +3,28 @@
 #? многочлена и вывести многочлен степени k.
 
 # Пример:
-# k = 2 => 2*x² + 4*x + 5 
-
+# k = 2 => 2*x² + 4*x + 5
 # k = 6 => ix^6 + ax^5 + bx^4 + cx^3 + dx^2 + ex + h
 # a, b, c, d, e, h - рандом
 
 from random import randint
 
+
+def polynom(lst):
+
+    result = []
+    for i in range(len(lst)):
+        if i == 0:
+            result.append(f'{lst[i]}')
+        elif i == 1:
+            result.append(f'{lst[i]}x')
+        else:
+            result.append(f'{lst[i]}x^{i}')
+
+    return ' + '.join(result[::-1])
+
+
 k = int(input('Input power: '))
+factors = [randint(0, 100) for _ in range(k+1)]
 
-lst = [randint(0, 100) for _ in range(k+1)]
-
-result = []
-for i in range(len(lst)):
-    if i == 0:
-        result.append(f'{lst[i]}')
-    elif i == 1:
-        result.append(f'{lst[i]}x')   
-    else:
-        result.append(f'{lst[i]}x^{i}')
-
-result = ' + '.join(result[::-1])
-
-print(result)
+print(polynom(factors))
